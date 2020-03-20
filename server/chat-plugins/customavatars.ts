c6fbf0172a9a03406c66bd5bf30a60bba982056a
Mia <49593536+mia-pi-git@users.noreply.github.com>
2020-03-20 14:21:15 -0500

    add customavatar plugin

---
+++ server/chat-plugins/customavatars.ts
@@ -0,0 +1,117 @@
+/** Original plugin from https://github.com/CreaturePhil/Showdown-Boilerplate/blob/master/chat-plugins/customavatar.js.
+Credits to CreaturePhil and the other listed contributors.
+updated for side-server use by Mia-pi. */
+
+
+import {FS} from '../../lib/fs';
+import * as fs from 'fs'; 
+import * as path from 'path';
+import * as https from 'https';
+
+const AVATAR_PATH = 'config/avatars/';
+
+const VALID_EXTENSIONS = ['.png'];
+
+export function downloadImage(image_url: string, name: string) {
+	return new Promise((resolve, reject) => {
+		https.get(image_url, function(response: any) {
+				if (response.statusCode !== 200) return;
+				let type = response.headers['content-type'].split('/');
+				if (type[0] !== 'image') return;
+				const stream = fs.createWriteStream(`${AVATAR_PATH}${name}.png`);
+        //no event listeners for ps streams so, normal fs has to be used.
+				response.pipe(stream);
+				stream.end();
+				stream.on('finish', () => { 
+					resolve('true');
+				});	
+		 });
+	});
+}
+
+export async function loadCustomAvatars() {
+	const avatars: string[] = [];
+	try {
+		const files = FS(AVATAR_PATH).readdirSync();
+		// passing over non-avatar files
+		for (const file of files) {
+			if (!file.endsWith('.png')) {
+				continue;
+			} else {
+				avatars.push(file);
+			}
+		}
+	} catch (e) {
+		throw new Error(e);
+	}
+	for (const file of avatars) {
+		const name = path.basename(file, path.extname(file));
+		Config.customavatars[name] = file;
+	}
+}
+
+loadCustomAvatars();
+
+export const commands: ChatCommands = {
+	customavatar: {
+		add(target, room, user, connection) {
+			if (!user.hasConsoleAccess(connection)) return false;
+			const parts = target.split(',').map(param => param.trim());
+			if (parts.length < 2) return this.parse('/help customavatar');
+
+			const name = toID(parts[0]);
+			const targetUser = Users.get(name);
+			let avatarUrl = parts[1];
+			if (!/^https?:\/\//i.test(avatarUrl)) avatarUrl = 'http://' + avatarUrl;
+			const ext = path.extname(avatarUrl);
+
+			if (!VALID_EXTENSIONS.includes(ext)) {
+				return this.errorReply("Image url must end in a .png extension.");
+			}
+			if (!avatarUrl.includes('https:') || avatarUrl.includes('http:')) { 
+				return this.errorReply("Image url must be https.");
+			}
+
+			Config.customavatars[name] = name + ext;
+
+			try {
+				downloadImage(avatarUrl, name);
+			} catch (e) {
+				this.errorReply(`Error in downloading image: ${e}`);
+			}
+			this.sendReply(`|raw|${name}${name.endsWith('s') ? "'" : "'s"} avatar was successfully set. Avatar:<br /><img src="${avatarUrl}" width="80" height="80">`);
+			Monitor.adminlog(`${name}'s avatar was successfully set by  ${user.name}.`);
+			const msg = `Upper staff have set your custom avatar.<br /><img src='${avatarUrl}' width='80' height='80'><br /> Refresh your page if you don't see it.`;
+			if (targetUser) targetUser.popup(`|html|${msg}`);
+		},
+
+		remove(target, room, user, connection) {
+			if (!user.hasConsoleAccess(connection)) return false;
+			const userid = toID(target);
+			const targetUser = Users.get(target);
+			const image = Config.customavatars[userid];
+
+			if (!image) return this.errorReply(target + " does not have a custom avatar.");
+
+			if (FS(AVATAR_PATH + image).existsSync()) {
+				delete Config.customavatars[userid];
+				FS(AVATAR_PATH + image).unlinkIfExists();
+				if (targetUser) targetUser.popup("Upper staff have removed your custom avatar.");
+				this.sendReply(target + "'s avatar has been successfully removed.");
+				Monitor.adminlog(target + "'s avatar has been successfully removed.");
+			} else {
+				this.errorReply("That custom avatar file does not exist - try again?");
+			}
+		},
+
+		customavatarhelp: 'help',
+		help(target, room, user) {
+			this.parse('/help customavatar');
+		},
+	},
+
+	customavatarhelp: [
+		"Commands for /customavatar are:",
+		"/customavatar add [username], [image link] - Set a user's custom avatar. Requires: & ~",
+		"/customavatar remove [username] - Delete a user's custom avatar. Requires: & ~"],
+};
